5bc688c0384d34834a560e4ca362ebdefc6b9424
AashilDesai <aashild@seas.upenn.edu>
2016-10-11 17:31:34 -0400

    Empty intentory and pokemon when adventure.sh is run

--- adventure.sh
+++ adventure.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "hello"
+inventoryFile='./windows/inventory.txt'
+pokemonFile='./windows/pokemon.txt'
+
+#At the start of the game, we wish to empty the player's inventory and pokemon
+
+>$inventoryFile
+>$pokemonFile
